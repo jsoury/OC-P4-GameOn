cf203ba1327c7d7baf01f0709bfdf93c72aa9d15
unknown <jonathan.soury@gmail.com>
2022-05-03 00:56:56 +0200

    ajout confirmation envoi + test manuels

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,62 +13,99 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const modalCloseBtn = document.querySelector(".close");
 const modalSubmitBtn = document.querySelector(".btn-submit");
 const form = document.forms.reserve;
-const allInput = document.querySelectorAll("input");
+const allInput = Array.prototype.slice.call(document.querySelectorAll("input"));
+const allInputFocusable = allInput.slice(0, 5);
+const allInputClickable = allInput.slice(5, 13);
 
 // launch modal form
 const launchModal = () => {
+  toggleButtonSubmit("open");
+  allInput.forEach((element) => {
+    toggleErrorMessages(element, false);
+  });
   modalbg.style.display = "block";
 };
 
 //close modal
 const closeModal = () => {
   modalbg.style.display = "none";
+  hideThanks();
+  form.reset();
 };
 
-// prevents paste
-form.onpaste = function (event) {
-  event.preventDefault();
+//hide thanks message and remove class disabled of elements formData
+const hideThanks = () => {
+  document.querySelectorAll(".formData").forEach((element) => {
+    element.classList.remove("disabled");
+  });
+  document.querySelector(".thanks").style.display = "none";
 };
-const test = () => {
-  console.log("test");
+
+//show thanks message and add class disabled of elements formData
+const showThanks = () => {
+  document.querySelectorAll(".formData").forEach((element) => {
+    element.classList.add("disabled");
+  });
+  document.querySelector(".thanks").style.display = "block";
+};
+
+const toggleButtonSubmit = (modal) => {
+  modal === "open"
+    ? ((modalSubmitBtn.value = "c'est parti"),
+      modalSubmitBtn.removeAttribute("onClick"))
+    : ((modalSubmitBtn.value = "Fermer"),
+      modalSubmitBtn.setAttribute("onClick", "closeModal();"));
+};
+
+/*
+ * call the validate function to check all the form fields
+ * if there is an error return false
+ * else return true
+ */
+const validateSubmit = (e) => {
+  let isValid = 0;
+  const validations = {
+    first: false,
+    last: false,
+    email: false,
+    birthdate: false,
+    quantity: false,
+    location: false,
+    cgu: false,
+  };
+  for (let validation in validations) {
+    validations[validation] = validate(validation);
+    if (validations[validation] === true) isValid += 1;
+  }
+  if (isValid === 7) {
+    showThanks();
+    toggleButtonSubmit("close");
+    //isValid = 0;
+  }
+  return false;
 };
 
 //switch fonction for valide values input
-const validate = (inputId) => {
-  console.log("validatee" + " " + inputId);
-  switch (inputId) {
+const validate = (input) => {
+  switch (input) {
     case "first":
-      validateFirstAndLast(inputId);
-      break;
+      return validateFirstAndLast(input);
     case "last":
-      validateFirstAndLast(inputId);
-      break;
+      return validateFirstAndLast(input);
     case "email":
-      validateEmail();
-      break;
+      return validateEmail();
+    case "birthdate":
+      return validateBirthDate();
     case "quantity":
-      validateQuantity();
+      return validateQuantity();
+    case "location":
+      return validateLocationCheked();
+    case "cgu":
+      return validateCguCheked();
+    case "mailing":
       break;
     default:
-      console.log(`${inputId} n'existe pas`);
-  }
-};
-
-const submit = (e) => {
-  console.log("submit");
-  let isValid = false;
-  inputIds = ["first", "last"];
-  for (let id of inputIds) isValid = validateFirstAndLast(id);
-  isValid = validateEmail();
-  isValid = validateQuantity();
-  console.log(isValid);
-  isValid = validateLocationCheked();
-  isValid = false;
-  if (!isValid) {
-    e.preventDefault();
-    alert("bloqué");
-    console.log("error");
-    return false;
+      console.log(`${input} n'existe pas`);
   }
 };
 
@@ -93,6 +130,14 @@ const validateEmail = () => {
     : (toggleErrorMessages(email, true), false);
 };
 
+//validate birthdate is not empty
+const validateBirthDate = () => {
+  const birthdate = form.elements.birthdate;
+  return birthdate.value != ""
+    ? (toggleErrorMessages(birthdate, false), true)
+    : (toggleErrorMessages(birthdate, true), false);
+};
+
 //validate quantity by regex
 const validateQuantity = () => {
   const quantity = form.elements.quantity;
@@ -102,6 +147,7 @@ const validateQuantity = () => {
     : (toggleErrorMessages(quantity, true), false);
 };
 
+//validate that the location is checked
 const validateLocationCheked = () => {
   const location = form.elements.location;
   for (let element of location) {
@@ -113,22 +159,41 @@ const validateLocationCheked = () => {
   return false;
 };
 
-//add class name error insert message after input parameters
+//validate that the cgu is checked
+const validateCguCheked = () => {
+  const cgu = form.elements.cgu;
+  if (cgu.checked) {
+    toggleErrorMessages(cgu, false);
+    return true;
+  } else {
+    toggleErrorMessages(cgu, true);
+    return false;
+  }
+};
+/*
+ * set the data-visible attribute to true or false
+ * I have to call the grandparents for the location ellement
+ * element = selected dom element
+ * etat = true(show), false(hide);
+ */
+
 const toggleErrorMessages = (element, etat) => {
-  element.parentNode.dataset.visible = etat;
+  if (element.name == "location") {
+    element.parentNode.parentNode.dataset.visible = etat;
+  } else {
+    element.parentNode.dataset.visible = etat;
+  }
 };
 
 //wait for the document to load
 document.addEventListener("DOMContentLoaded", (e) => {
-  // launch modal event
+  // event listener for the click on the "I register" button
   modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-  // close modal event
+  // event listener for the click on the "close" button
   modalCloseBtn.addEventListener("click", closeModal);
-  // button submit modal event
-  form.addEventListener("submit", submit);
 
   //add blur, focus and click on all input elements
-  allInput.forEach((input) => {
+  allInputFocusable.forEach((input) => {
     input.addEventListener(
       "blur",
       (e) => {
@@ -143,8 +208,10 @@ document.addEventListener("DOMContentLoaded", (e) => {
       },
       false
     );
-    // input.addEventListener("click", (e) => {
-    //   validateLocationCheked();
-    // });
+  });
+  allInputClickable.forEach((input) => {
+    input.addEventListener("click", (e) => {
+      validate(e.currentTarget.name);
+    });
   });
 });
